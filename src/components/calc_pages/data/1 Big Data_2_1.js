c4804744e8d30190b28dc8a7c777a0e19ed8f8bf
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2023-09-08 22:31:36 +0100

    rename data

---
+++ src/components/calc_pages/data/1 Big Data_2_1.js
@@ -0,0 +1,25 @@
+export const data = {
+    regmix: {
+      placeholder: ["DS x 0.3", "Exam x 0.7"],
+      coef: [0.3, 0.7],
+      subjects: [
+        { name: "Algèbre1 (1.5) ", coef: 1.5, grades: [] },
+        { name: "Analyse1 (1.5)", coef: 1.5, grades: [] },
+        { name: "Algo (2)", coef: 2, grades: [] },
+        { name: "C (1.5)", coef: 1.5, grades: [] },
+        { name: "SE2 (1.5)", coef: 1.5, grades: [] },
+        { name: "Sys Log (2)", coef: 2, grades: [] },
+        { name: "Log Formelle (1.5)", coef: 1.5, grades: [] },
+        { name: "TMM (1.5)", coef: 1.5, grades: [] },
+      ],
+    },
+    cc: {
+      placeholder: ["DS1 x 0.4", "DS2 x 0.4", "CC x 0.2"],
+      coef: [0.4, 0.4, 0.2],
+      subjects: [
+        { name: "Anglais1 (1)", coef: 1, grades: [] },
+        { name: "Tech de Comm (1)", coef: 1, grades: [] },
+      ],
+    },
+  };
+  
