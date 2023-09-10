adfd985aa80752d349ef5c173c6bec4e1d22034b
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2023-09-10 10:54:36 +0100

    social_i_les

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,16 +1,14 @@
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
-import navIcon4 from "../assets/img/twitter-svgrepo-com.svg";
 import navIcon5 from "../assets/img/github-mark.svg";
 import navIcon6 from "../assets/img/website-ui-svgrepo-com.svg";
 
 export const ContactMe = () => {
   return (
     <footer className="footer">
-      <h3 className="footer--title">Contact Me</h3>
+      <h3 className="footer--title">Reach Me</h3>
       <p className="footer--text">
-        You can contact me on my social media accounts
+        You can view my portfolio website, my LinkedIn profile, You can also
+        find my code on GitHub
       </p>
       <div className="social-icon">
         <a href="https://ifiras.pages.dev/" target="blank">
@@ -21,15 +19,6 @@ export const ContactMe = () => {
           target="blank"
         >
           <img src={navIcon1} alt="Icon" title="LinkedIn" />
-        </a>
-        <a href="https://www.facebook.com/firas.kahlaoui.566/" target="blank">
-          <img src={navIcon2} alt="Icon" title="Facebook" />
-        </a>
-        <a href="https://www.instagram.com/firas_kahlaouii/" target="blank">
-          <img src={navIcon3} alt="Icon" title="Instagram" />
-        </a>
-        <a href="https://twitter.com/firas_kahlaoui" target="blank">
-          <img src={navIcon4} alt="Icon" title="Twitter" />
         </a>
         <a href="https://github.com/FirasKahlaoui" target="blank">
           <img src={navIcon5} alt="" title="GitHub" />
