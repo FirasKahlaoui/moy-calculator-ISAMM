1751fb962135af82c24ca7195146c6ee79b2680f
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2023-09-15 11:01:06 +0100

    Dot_Loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import Home from "./Home"
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      
     </div>
   );
 }
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,15 +1,35 @@
 import { NavBar } from "../components/NavBar";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import DotLoader from "react-spinners/DotLoader";
 
 export const Result = () => {
   const location = useLocation();
   const average = location.state.average;
+  const [loading, setLoading] = useState(false);
+  const [loadingText, setLoadingText] = useState("");
+  useEffect(() => {
+    setLoading(true);
+    setLoadingText("Calculating...");
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <div>
-      <NavBar />
-      <h1>Result</h1>
-      <p>Weighted average: {average}</p>
+      {loading ? (
+        <div className="loader">
+          <DotLoader color={"#ffffff"} loading={loading} size={90} />
+          <div className="loading-text">{loadingText}</div>
+        </div>
+      ) : (
+        <>
+          <NavBar />
+          <h1>Result</h1>
+          <p>Weighted average: {average}</p>
+        </>
+      )}
     </div>
   );
 };
