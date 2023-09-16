b1bf610a7b3f24a961856c6b9bfbcaa11a8cfb3e
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2023-09-16 23:59:59 +0100

    1CM

---
+++ src/components/calc_pages/data/1 CM_3_1.js
@@ -0,0 +1,32 @@
+export const data = {
+  regmix: {
+    name: "regmix",
+    placeholder: ["Exam x 0.7", "DS x 0.3"],
+    coef: [0.7, 0.3],
+    subjects: [
+      {
+        name: "Fondamentale théorique du multimédia 1 (1) ",
+        coef: 1,
+        grades: [],
+      },
+      { name: "Algorithme(1) ", coef: 1, grades: [] },
+      { name: "Géneralités informatique(1) ", coef: 1, grades: [] },
+      { name: "Introduction a la comm multi 1 (1)", coef: 1, grades: [] },
+      { name: "Conception et outils pour le web 1 (2)", coef: 2, grades: [] },
+      { name: "Eléments fonda de la musique 1 (1)", coef: 1, grades: [] },
+      { name: "Technologie et outils de création 1 (2)", coef: 2, grades: [] },
+    ],
+  },
+  cc: {
+    name: "cc",
+    placeholder: ["DS1 x 0.4", "DS2 x 0.4", "CC x 0.2"],
+    coef: [0.4, 0.4, 0.2],
+    subjects: [
+      { name: "Techniques d'expression 1 (1)", coef: 1, grades: [] },
+      { name: "Activité pratiques (2)", coef: 2, grades: [] },
+      { name: "Compétences linguistiques Francais(1)", coef: 1, grades: [] },
+      { name: "Compétences linguistiques Anglais(1)", coef: 1, grades: [] },
+      { name: "Compétences digitales(1)", coef: 1, grades: [] },
+    ],
+  },
+};
