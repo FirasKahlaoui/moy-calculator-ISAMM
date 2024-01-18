19640eed58c2bc5b897f434d6ad085af201905a4
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2024-01-18 15:51:14 +0100

    fix 3bd sem 1

--- src/components/calc_pages/data/3 Big Data_2_1.js
+++ src/components/calc_pages/data/3 Big Data_2_1.js
@@ -4,6 +4,14 @@ export const data = {
     placeholder: ["Exam x 0.7", "DS x 0.3"],
     coef: [0.7, 0.3],
     subjects: [
+      { name: "Entrepôts de données (1.5)", coef: 1.5, grades: [] },
+      { name: "Techniques de veille (1.5)", coef: 1.5, grades: [] },
+      { name: "Bases de données NoSQL (1.5)", coef: 1.5, grades: [] },
+      {
+        name: "Sécurité & contrôle d'accès aux Big data (1.5)",
+        coef: 1.5,
+        grades: [],
+      },
       { name: "Techniques de prévision (1.5)", coef: 1.5, grades: [] },
       { name: "Visualisation des données (1.5)", coef: 1.5, grades: [] },
     ],
@@ -13,14 +21,6 @@ export const data = {
     placeholder: ["DS1 x 0.4", "DS2 x 0.4", "CC x 0.2"],
     coef: [0.4, 0.4, 0.2],
     subjects: [
-      { name: "Entrepôts de données (1.5)", coef: 1.5, grades: [] },
-      { name: "Techniques de veille (1.5)", coef: 1.5, grades: [] },
-      { name: "Bases de données NoSQL (1.5)", coef: 1.5, grades: [] },
-      {
-        name: "Sécurité & contrôle d'accès aux Big data (1.5)",
-        coef: 1.5,
-        grades: [],
-      },
       { name: "Anglais 4 (1.5)", coef: 1.5, grades: [] },
       { name: "Entreprenariat (1.5)", coef: 1.5, grades: [] },
       { name: "Framework de développement web (1.5)", coef: 1.5, grades: [] },
