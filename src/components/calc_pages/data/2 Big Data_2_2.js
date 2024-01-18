16d7596df0548663780e05063fad8db9ddbdbd04
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2024-01-18 15:49:29 +0100

    fix 2bd sem2

--- src/components/calc_pages/data/2 Big Data_2_2.js
+++ src/components/calc_pages/data/2 Big Data_2_2.js
@@ -4,20 +4,12 @@ export const data = {
     placeholder: ["Exam x 0.7", "DS x 0.3"],
     coef: [0.7, 0.3],
     subjects: [
-      { name: "Analyse & fouille de données (2)", coef: 2, grades: [] },
-      { name: "Fondament & Programmation AI (1.5)", coef: 1.5, grades: [] },
-    ],
-  },
-  cc: {
-    name: "cc",
-    placeholder: ["DS1 x 0.4", "DS2 x 0.4", "CC x 0.2"],
-    coef: [0.4, 0.4, 0.2],
-    subjects: [
       {
         name: "Statistiques descriptives & inférentielles (1.5)",
         coef: 1.5,
         grades: [],
       },
+      { name: "Analyse & fouille de données (2)", coef: 2, grades: [] },
       {
         name: "Big data & architectures associées (1.5)",
         coef: 1.5,
@@ -28,6 +20,14 @@ export const data = {
         coef: 1.5,
         grades: [],
       },
+      { name: "Fondament & Programmation AI (1.5)", coef: 1.5, grades: [] },
+    ],
+  },
+  cc: {
+    name: "cc",
+    placeholder: ["DS1 x 0.4", "DS2 x 0.4", "CC x 0.2"],
+    coef: [0.4, 0.4, 0.2],
+    subjects: [
       { name: "Projet fédéré [méthode Agile] (1)", coef: 1, grades: [] },
       {
         name: "Droit informatique, protection des données & éthique (1)",
